ab55c8de3b29cd1b5f1c1366de595f1bfd3a698a
Roman <Roman-graf2000@gmx.at>
2021-06-28 01:13:00 +0200

    Complete Fee class

--- app/src/main/java/com/example/feeasy/Fee.java
+++ app/src/main/java/com/example/feeasy/Fee.java
@@ -1,4 +1,18 @@
 package com.example.feeasy;
 
+import java.util.Date;
+
 public class Fee {
+    Group group;
+    GroupMember groupMember;
+    float amount;
+    Date date;
+
+    public Fee(Group group, GroupMember groupMember, float amount, Date date) {
+        this.group = group;
+        this.groupMember = groupMember;
+        this.amount = amount;
+        this.date = date;
+    }
+
 }
