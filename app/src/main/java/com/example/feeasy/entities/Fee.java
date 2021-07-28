9b325b9944b8fa1e6e9005a56c57398795e53ec5
Roman <Roman-graf2000@gmx.at>
2021-07-29 00:55:20 +0200

    Add IDs to Fees

--- app/src/main/java/com/example/feeasy/entities/Fee.java
+++ app/src/main/java/com/example/feeasy/entities/Fee.java
@@ -1,6 +1,7 @@
 package com.example.feeasy.entities;
 
 public class Fee {
+    public int id;
     public String name;
     public Group group;
     public GroupMember groupMember;
@@ -8,7 +9,8 @@ public class Fee {
     public String date;
     public FeeStatus status;
 
-    public Fee(String name, Group group, GroupMember groupMember, float amount, String date) {
+    public Fee(int id, String name, Group group, GroupMember groupMember, float amount, String date) {
+        this.id = id;
         this.name = name;
         this.group = group;
         this.groupMember = groupMember;
