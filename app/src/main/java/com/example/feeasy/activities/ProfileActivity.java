030afb0f6bf5660a560d4fee591dc597681161e7
Roman <Roman-graf2000@gmx.at>
2021-07-31 21:08:34 +0200

    Add Dialog to Sign out

--- app/src/main/java/com/example/feeasy/activities/ProfileActivity.java
+++ app/src/main/java/com/example/feeasy/activities/ProfileActivity.java
@@ -1,10 +1,15 @@
 package com.example.feeasy.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -54,6 +59,33 @@ public class ProfileActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.profile_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.profile_menu_logout){
+            openDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        dataManager.signOut();
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openDialog(){
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        b.setTitle("Sign out?");
+        b.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        b.setNegativeButton("CANCEL", null);
+        b.show();
     }
 
     public void updateDisplayname(JSONObject jsonObject){
